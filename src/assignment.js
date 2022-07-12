9055d132d8c3d3471b99a8efdc77a69cf7853edd
ogunleye <104855549+ogunleye0720@users.noreply.github.com>
2022-07-12 16:02:55 +0000

    changes made to challenge-1 and challenge-2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,26 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
+/*
+The function sumOfNumbers returns the sum of numbers in the arrayOfNumbers.
+The for loop was used to iterate through the array and passed  10 posoitions (0 to 9) to the Counter variable.
+An if statement was used to check if the value in the array is not a number, if true, the element is skipped.
+*/
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (let counter = 0 ; counter < arrayOfNumbers.length ; counter++){
+        if (isNaN(arrayOfNumbers[counter])){
+            continue;
+        }
+        else {
+            arrayOfNumbers[counter] =  arrayOfNumbers[counter];
+        }
+        sum += arrayOfNumbers[counter];
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +43,29 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+/*
+The function countEvenNumbers returns the number of even numbers in the arrayOfNumers.
+The for loop was used to iterate through the array and passed  10 posoitions (0 to 9) to the Counter variable.
+An if statement was used to check if the value in the array is not a number, if true, the element is skipped.
+and an else if condition was used to check if the value of element in array is not an even number, if true, the element is skipped.
+*/
+function countEvenNumbers(arrayOfNumbers){
+    let evenSum = 0;
+    for (let evenCounter = 0; evenCounter < arrayOfNumbers.length; evenCounter++){
+      if (isNaN(arrayOfNumbers[evenCounter])){
+            continue;
+        }
+        else if (arrayOfNumbers[evenCounter] % 2 !== 0){
+            continue;
+        }
+      else{
+        evenSum++;
+      }
+    }
+  return evenSum;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
